a5a5be7d3000f73898fd171a3be1c1c56ac27ad5
Kakashidota <robin.kamo@live.se>
2023-09-20 14:41:58 +0200

    Add project files.

---
+++ CoffeFriend/CoffeQueue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeFriend
+{
+    internal class CoffeQueue
+    {
+
+        private Queue<Person> coffeQue = new Queue<Person>();
+
+
+        //Add person to the queue
+        public void Enqueue(Person person)
+        {
+            coffeQue.Enqueue(person);
+        }
+
+        //Removes the first person in the quue
+        public Person Dequeue()
+        {
+            return coffeQue.Dequeue();
+        }
+
+        public void Rotate()
+        {
+            if(coffeQue.Count > 1)
+            {
+                var firstPerson = coffeQue.Dequeue();
+                coffeQue.Enqueue(firstPerson);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", coffeQue);
+        }
+    }
+}
---
+++ CoffeFriend/Program.cs
@@ -0,0 +1,51 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace CoffeFriend
+{
+    internal class Program
+    {
+        static CoffeQueue coffe = new CoffeQueue();
+        static async Task Main(string[] args)
+        {
+            var discordClient = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.All
+            });
+
+            discordClient.MessageCreated += OnMessageCreated;
+            await discordClient.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+
+        private static async Task OnMessageCreated(DiscordClient client, MessageCreateEventArgs msg)
+        {
+            
+            //1. Adding a person to the queue
+            if(string.Equals(msg.Message.Content, "!add", StringComparison.OrdinalIgnoreCase))
+            {
+                string name = msg.Author.Username;
+                Person person = new Person(name);
+                coffe.Enqueue(person);
+                await msg.Message.RespondAsync($"{name} has been added to the queue");
+            }
+
+            //2. Checking the current queue
+            if(string.Equals(msg.Message.Content, "!check", StringComparison.OrdinalIgnoreCase))
+            {
+                await msg.Message.RespondAsync(coffe.ToString());
+            }
+
+            //3. Rotates the list by taking out the first person and then adding the same person to the end of the list
+            if(string.Equals(msg.Message.Content, "!rotate", StringComparison.OrdinalIgnoreCase))
+            {
+                coffe.Rotate();
+                await msg.Message.RespondAsync(coffe.ToString());
+            }
+
+        }
+    }
+}
